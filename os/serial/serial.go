f506aca64ae33eab951493aa56cc6b1a229aef58
钱纯净 <1113655791@qq.com>
2024-11-28 23:41:09 +0800

    101

--- os/serial/serial.go
+++ os/serial/serial.go
@@ -6,8 +6,9 @@ type Serial struct {
 	machine.Serialer
 }
 
+// Read 实现io.Reader接口
 func (this *Serial) Read(p []byte) (n int, err error) {
-	for n := 0; n < len(p) || n < this.Buffered(); n++ {
+	for n = 0; n < len(p) || n < this.Buffered(); n++ {
 		p[n], err = this.Serialer.ReadByte()
 		if err != nil {
 			return 0, err
